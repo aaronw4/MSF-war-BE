ee912fb806f53457747c20cc9ea1509f028d72fd
Aaron Williams <aaronrw4@yahoo.com>
2021-12-17 14:40:37 -0800

    Created findById model and update model

--- models/charactersModel.js
+++ models/charactersModel.js
@@ -4,19 +4,32 @@ function getCharacters() {
     return db('characters')
 };
 
+function findById(id) {
+    return db('characters')
+        .where({id})
+};
+
 function add(post) {
     return db('characters')
         .insert(post)
 };
 
+function update(changes, id) {
+    return db('characters')
+        .where({id})
+        .update(changes)
+}
+
 function remove(id) {
     return db('characters')
         .where({id})
         .delete()
-}
+};
 
 module.exports = {
     getCharacters,
+    findById,
     add,
+    update,
     remove
-}
+};
